6beffdb6b104b629dc4aa40d0eb50cd0d4fa918f
Dulio Denis <dulio.denis@yahoo.com>
2018-02-09 21:08:27 -0500

    Use custom image classification model converted from OS Caffe model

--- FlowerPower/FlowerPower/Controllers/CameraController.swift
+++ FlowerPower/FlowerPower/Controllers/CameraController.swift
@@ -7,7 +7,67 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class CameraController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary //.camera
+        imagePicker.allowsEditing = false
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Loading Core ML Model Failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let classification = request.results?.first as? VNClassificationObservation else {
+                fatalError("Model failed to process image.")
+            }
+            
+            self.navigationItem.title = classification.identifier.capitalized
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+}
+
+
+extension CameraController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = userPickedImage
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                // we could not convert the UIImage to a CIImage
+                fatalError("Error converting image to a Core Image.")
+            }
+            
+            detect(image: ciImage)
+        }
+        
+        imagePicker.dismiss(animated: true)
+    }
+    
 }
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Flower Power
 Flower Power is an iOS 11 / Swift 4 based image recognition app that uses the Oxford 102 Category dataset as a pre-trained model for Core ML to classify what type of flower is in your photo.
 
+![](art/screenshot/flowerpower-01.png?raw=true)
+
 ### Contributing
 See [CONTRIBUTING](CONTRIBUTING.md).
 
